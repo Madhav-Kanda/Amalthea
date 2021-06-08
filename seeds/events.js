1729ecd4d79243675ef0f73df48ff4a4b2d89ab4
Madhav-Kanda <76394914+Madhav-Kanda@users.noreply.github.com>
2021-06-08 23:47:00 +0530

    Add files via upload

---
+++ seeds/events.js
@@ -0,0 +1,47 @@
+module.exports=[
+    {
+        name:"Cryptorush",
+        about:"Crypto-Trading: A business to make easy money by making a few clicks on time! Is it so? Check out what graph crunching and timely clicks real Crypto-Trading takes, right here in CryptoRush. We bring forward an opportunity to feel and act like a real Crypto-Trader without any risks. Here's how you sum it up. Mock Money, Real-Time Rates, Real Brokerage Rates and Real courage to make a worthy investment!",
+        contact1name:"Adarsh Golait",
+        contact1email:" adarsh.g@iitgn.ac.in",
+        contact1phone:" +91 8609772536",
+        contact2name:"Sayan Biswas",
+        contact2email:"biswas.sayan@iitgn.ac.in",
+        contact2phone:"+91 7998084835",
+        image:"https://amalthea.iitgn.ac.in/images/cryptorush-100.jpg"
+    },
+    {
+        name:"Chem-e-Think",
+        about:"An interesting quizzing competition for all chemical engineering enthusiasts out there. This event encourages participants to ponder upon their theoretical knowledge of chemical engineering and chemistry while thinking out of the box.",
+        contact1name:"Sresth Tosniwal",
+        contact1email:"sresth.t@iitgn.ac.in",
+        contact1phone:"+91 9429530407",
+        contact2name:"Shubhi Maheshwari",
+        contact2email:"shubhi.maheshwari@iitgn.ac.in",
+        contact2phone:"+91 8290701802" ,
+        image:"https://amalthea.iitgn.ac.in/images/chemethink.png"
+    },
+    {
+        name:"Cryptix",
+        about:"CryptixCTF is a jeopardy-style capture the flag event which requires teams to solvesome tasks to find hidden flags. The tasks involve application of knowledge in the field of Cryptography, Steganography, Reverse engineering and Web exploitation. Though it may seem a bit fancy to some, the contest is designed while keeping beginners in mind. So, put on your hacker hats and showcase your hacking skills!",
+        contact1name:"Abhigyan Martin Ninama",
+        contact1email:"abhigyan.mn@iitgn.ac.in",
+        contact1phone:"+91 9406589979",
+        contact2name:"Shubhi Maheshwari",
+        contact2email:"shubhi.maheshwari@iitgn.ac.in",
+        contact2phone:"+91 8290701802",
+        image:"https://amalthea.iitgn.ac.in/images/CRYPTIX.png" 
+    },
+    {
+        name:"GameJam 2020 AD",
+        about:"Game Jam 2020 AD is a game development competition in which participants are expected to make a game in 2 weeks based on a theme.",
+        contact1name:"Reuben Devanesna",
+        contact1email:"reuben.sd@iitgn.ac.in",
+        contact1phone:"+91 9867608536",
+        contact2name:"Shubhi Maheshwari",
+        contact2email:"shubhi.maheshwari@iitgn.ac.in",
+        contact2phone:"+91 8290701802",
+        image:"https://amalthea.iitgn.ac.in/images/gamejam-100.jpg" 
+    },
+
+]
